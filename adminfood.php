51467dfc5f4f7fdfee700a79c1a743137027512f
DanLTX <166453442+DanLTX@users.noreply.github.com>
2024-06-23 09:54:43 +0800

    Update adminfood.php

--- adminfood.php
+++ adminfood.php
@@ -1,5 +1,20 @@
 <?php
 include 'conn.php';
+session_start();
+$email = $_SESSION["email"];
+$sqlauth = "SELECT * FROM admin";
+$result = mysqli_query($conn, $sqlauth);
+if (mysqli_num_rows($result) == 1) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $auth = $row["emailID"];
+    }
+}
+if (!isset($_SESSION['email']) || $_SESSION['email'] != $auth) {
+    header('Location: login.php');
+    session_destroy();
+    exit();
+}
 $sql = " SELECT * FROM food ORDER BY foodName ASC ";
 $result = $conn->query($sql);
 $conn->close();
